d8658018e1648a9ca644d1d237b3f5e0a50254f9
O Rei dos Hackers <rei@reinohacker.com>
2023-10-27 14:24:43 -0300

    Adicionado função de verificar portas abertas

--- mata-virus-rh.sh
+++ mata-virus-rh.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-killall -9 freshclam
+
 clear
+
 
 function apresenta_banner() {
         echo "        v_0.2"
@@ -23,19 +24,35 @@ function apresenta_banner() {
 	sleep 1
 }
 
+function permissao_root(){
+	# Pergunta ao usuário se deseja executar como root
+	echo -e "\033[1;31mAtenção: Essa ferramenta exige privilégios de root"
+
+	# Verifica root
+	if [[ $EUID -ne 0 ]]; then
+		echo -e "\033[0mReiniciando e usando sudo..."
+		sudo "$0" "$@"
+		exit
+	else
+		echo "Iniciando..."
+		echo "Eliminando processos concorrentes"
+		killall -9 freshclam
+	fi
+}
+
 
 function check_clamscan() {
     if ! command -v clamscan &>/dev/null; then
         read -p "ClamAV não está instalado. Pressione (enter) para instalar ou (ctrl+c) para sair."
-        sudo apt install clamav -y
+        apt install clamav -y
     fi
 }
 
 function update_clamav_database() {
     echo -e "\033[0;32m[+] Atualizando banco de dados do ClamAV...\033[0m"
-    sudo freshclam
+    freshclam
     echo -e "\033[0;32m[+] Atualizado com sucesso.\033[0;32m"
-    sleep 3
+    sleep 5
     clear
 }
 
@@ -79,7 +96,11 @@ function scan_file() {
             fi
         else
             echo "O arquivo não está infectado."
-            sleep 3
+
+	    # Aguarda o usuário pressionar Enter para concluir
+	    echo -e "\033[1;33m"
+	    read -p "Pressione Enter para concluir..."
+
             clear
 	fi
 }
@@ -108,7 +129,11 @@ function scan_directory() {
             fi
         else
             echo "Não foram encontrados arquivos infectados."
-            sleep 3
+
+	    # Aguarda o usuário pressionar Enter para concluir
+	    echo -e "\033[1;33m"
+	    read -p "Pressione Enter para concluir..."
+
             clear
         fi
     fi
@@ -119,10 +144,43 @@ function scan_system() {
         update_progress &
         progress_pid=$!
 
-        sudo clamscan --bell -r /
+        clamscan --bell -r /
 
         kill "$progress_pid"  # Parar o contador de progresso
+        
+	# Aguarda o usuário pressionar Enter para concluir
+	echo -e "\033[1;33m"
+	read -p "Pressione Enter para concluir..."
+
         clear
+}
+
+function check_ports(){
+	#!/bin/bash
+
+	# Verifica se o usuário é root (necessário para utilizar o nmap)
+	if [[ $EUID -ne 0 ]]; then
+	    echo "Esse recurso precisa ser executado como root."
+	    exit 1
+	fi
+
+	# Verifica se o nmap está instalado e instala se não estiver
+	if ! command -v nmap &> /dev/null; then
+	    echo "O Nmap não está instalado. Instalando..."
+	    apt-get update
+	    apt-get install -y nmap
+	fi
+
+	# Executa o Nmap para encontrar as portas abertas no localhost
+	echo -e "Procurando por portas abertas...\033[0;32m"
+	nmap -sT -O localhost | awk '/^ *[0-9]+/ {print "Porta:", $1,"| Estado:", $2,"| Serviço:", $3}'
+
+	# Aguarda o usuário pressionar Enter para concluir
+	echo -e "\033[1;33m"
+	read -p "Pressione Enter para concluir..."
+	clear
+
+
 }
 
 function main_menu() {
@@ -133,14 +191,17 @@ function main_menu() {
         echo "1- Escanear arquivo"
         echo "2- Escanear diretório"
         echo "3- Escanear todo o sistema"
-        echo "4- Sair"
+        echo "4- Checar Portas Abertas"
+        echo "5- Sair"
         read -p "Digite o número correspondente ao método desejado: " method
+        echo ""
 
         case $method in
             1) scan_file ;;
             2) scan_directory ;;
             3) scan_system ;;
-            4) echo "Saindo..." && exit 0 ;;
+            4) check_ports ;;
+            5) echo "Saindo..." && exit 0 ;;
             *) echo "Opção inválida. Tente novamente." ;;
         esac
     done
@@ -149,6 +210,7 @@ function main_menu() {
 # Função principal
 function main() {
     apresenta_banner
+    permissao_root
     check_clamscan
     update_clamav_database
     main_menu
